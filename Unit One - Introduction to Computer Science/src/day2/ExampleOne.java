9cc80d306526010b72363ea87187749b39dd7623
Sofia Giavedoni <sgiavedoni@bayviewglen.ca>
2022-09-09 11:07:24 -0400

    Initial commit

---
+++ Unit One - Introduction to Computer Science/src/day2/ExampleOne.java
@@ -0,0 +1,30 @@
+package day2;
+
+/**
+ * ExampleOne
+ */
+public class ExampleOne {
+public static void main(String[] args) {
+    System.out.println("Welcome to Computer Science");
+    System.out.println("Welcome to Computer Science");
+    System.out.println("Welcome to Computer Science");
+
+    System.out.println();
+
+    System.out.print("A");
+    System.out.print("B");
+    System.out.print("C");
+    System.out.println("D");
+    System.out.print("E");
+    System.out.println("F");
+
+    // ABCD
+    //EF
+    /*
+     * Comment test
+     * Comments r ignored by the commpilelr (Java) and are meant for developers to understand what the code is 
+     */
+    
+}
+    
+}
---
+++ Unit One - Introduction to Computer Science/src/day2/ExampleTwo.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class ExampleTwo {
+public static void main(String[] args) {
+    System.out.println("Computer Scie\nce is the \"BEST\" class ever!");
+    System.out.println("/\\\"");
+}
+}
+// escape sequence is meant to give another meaning to a character
+// AP Exam cares about \\ \" and \n
+// \" displaysa a quatoe
+// // displays a backslash"
