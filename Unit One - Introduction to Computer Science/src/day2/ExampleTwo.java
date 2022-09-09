4e518cda725ef944c3859c02e528d3388cc5f40e
Sofia Giavedoni <sgiavedoni@bayviewglen.ca>
2022-09-09 11:10:40 -0400

    testing git

--- Unit One - Introduction to Computer Science/src/day2/ExampleTwo.java
+++ Unit One - Introduction to Computer Science/src/day2/ExampleTwo.java
@@ -4,6 +4,7 @@ public class ExampleTwo {
 public static void main(String[] args) {
     System.out.println("Computer Scie\nce is the \"BEST\" class ever!");
     System.out.println("/\\\"");
+    // test git
 }
 }
 // escape sequence is meant to give another meaning to a character
